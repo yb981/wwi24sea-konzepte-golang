b916af578bd7ac5ede136cd734c28b67b0718645
Lukas Groening <113829864+yb981@users.noreply.github.com>
2025-02-22 16:13:24 +0100

    refactoring: moved funcs into calculator

--- Lukas/calculator.go
+++ Lukas/calculator.go
@@ -1,3 +1,159 @@
 package main
 
-type calculator []string
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+type calculator struct {
+	numberStack Stack[float64]
+	history     Stack[string]
+	latex       LaTeXOutput
+}
+
+func (c *calculator) checkInput(input string) {
+	// commands
+	switch input {
+	case "exit":
+		fmt.Println("\nQuitting Application. See you soon!")
+		os.Exit(0)
+	case "list":
+		fmt.Println("printing input")
+		c.numberStack.Print()
+		return
+	case "latex":
+		c.latex.Expression = c.history.Top()
+		fmt.Println(c.history.Top())
+		fmt.Println(c.latex.Expression)
+		fmt.Println(c.latex.formatToLatex(c.latex.Expression))
+		return
+	case "help":
+		c.printWelcomeMessage()
+		return
+	}
+
+	// operations handling
+	switch input {
+	case "+", "-", "*", "/", "^":
+		c.performBinaryOperation(input)
+	case "abs", "sqrt", "log":
+		c.performUnaryOperation(input)
+	case "++":
+		c.performSumOperation()
+	case "**":
+		c.performProductOperation()
+	default:
+		c.handleNumberInput(input)
+	}
+}
+
+func (c *calculator) performBinaryOperation(op string) {
+	term2 := c.history.Pop()
+	term1 := c.history.Pop()
+	var result float64
+
+	switch op {
+	case "+":
+		result = c.numberStack.Pop() + c.numberStack.Pop()
+	case "-":
+		secondOp := c.numberStack.Pop()
+		firstOp := c.numberStack.Pop()
+		result = firstOp - secondOp
+	case "*":
+		result = c.numberStack.Pop() * c.numberStack.Pop()
+	case "/":
+		secondOp := c.numberStack.Pop()
+		firstOp := c.numberStack.Pop()
+		result = firstOp / secondOp
+	case "^":
+		exponent := c.numberStack.Pop()
+		base := c.numberStack.Pop()
+		result = math.Pow(base, exponent)
+	}
+
+	termNew := fmt.Sprintf("(%s %s %s)", term1, op, term2)
+	c.history.Push(termNew)
+	fmt.Printf("current calculation: %s = %v\n", termNew, result)
+	c.numberStack.Push(result)
+}
+
+func (c *calculator) performUnaryOperation(op string) {
+	term1 := c.history.Pop()
+	var result float64
+
+	switch op {
+	case "abs":
+		result = math.Abs(c.numberStack.Pop())
+		c.history.Push(fmt.Sprintf("abs(%s)", term1))
+	case "sqrt":
+		result = math.Sqrt(c.numberStack.Pop())
+		c.history.Push(fmt.Sprintf("sqrt(%s)", term1))
+	case "log":
+		result = math.Log(c.numberStack.Pop())
+		c.history.Push(fmt.Sprintf("log(%s)", term1))
+	}
+	fmt.Printf("current calculation: %s = %v\n", c.history.Top(), result)
+	c.numberStack.Push(result)
+}
+
+func (c *calculator) performSumOperation() {
+	n := len(c.numberStack)
+	result := 0.0
+	// Eine Kopie des Stacks erstellen, damit wir zur Berechnung nicht die Werte verlieren.
+	for i := 0; i < n; i++ {
+		result += c.numberStack.Pop()
+	}
+	c.numberStack.Push(result)
+	fmt.Printf("current calculation: sum = %v\n", result)
+}
+
+func (c *calculator) performProductOperation() {
+	n := len(c.numberStack)
+	result := 1.0
+	// Eine Kopie des Stacks erstellen, damit wir zur Berechnung nicht die Werte verlieren.
+	for i := 0; i < n; i++ {
+		result *= c.numberStack.Pop()
+	}
+	if n > 0 {
+		c.numberStack.Push(result)
+		fmt.Printf("current calculation: product = %v\n", result)
+	}
+}
+
+func (c *calculator) handleNumberInput(input string) {
+	number, err := strconv.ParseFloat(input, 64)
+	if err == nil {
+		c.numberStack.Push(number)
+		c.history.Push(input)
+	} else {
+		// Fehlerbehandlung
+	}
+}
+
+func (c calculator) printWelcomeMessage() {
+	fmt.Println("=========================================")
+	fmt.Println("        Welcome to the RPN Calculator   ")
+	fmt.Println("=========================================")
+	fmt.Println("Functionality:")
+	fmt.Println(" +  Addition")
+	fmt.Println(" -  Subtraction")
+	fmt.Println(" *  Multiplication")
+	fmt.Println(" /  Division")
+	fmt.Println(" ^  Exponentiation")
+	fmt.Println(" sqrt  Square Root")
+	fmt.Println(" log  Logarithm (Base 10)")
+	fmt.Println(" !  Factorial")
+	fmt.Println(" abs  Absolute Value")
+	fmt.Println(" ++  Sum All Numbers on the Stack")
+	fmt.Println(" **  Multiply All Numbers on the Stack")
+	fmt.Println()
+	fmt.Println("Available Commands:")
+	fmt.Println(" - Type 'help' for assistance")
+	fmt.Println(" - Type 'latex' for LaTeX formatted output")
+	fmt.Println(" - Type 'exit' to close the application")
+	fmt.Println()
+	fmt.Println("=========================================")
+	fmt.Println("Please enter your command:")
+}
--- Lukas/main.go
+++ Lukas/main.go
@@ -1,67 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"math"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
+	calculator := calculator{}
 
-	numberStack := Stack[float64]{}
-	latex := LaTeXOutput{}
-	history := Stack[string]{}
-
-	printWelcomeMessage()
+	calculator.printWelcomeMessage()
 
 	for {
-		numberStack.Print()
+		calculator.numberStack.Print()
 
 		input := getInput()
 
-		checkInput(input, &numberStack, &latex, &history)
+		calculator.checkInput(input)
 	}
-}
-
-func printWelcomeMessage() {
-	fmt.Println("=========================================")
-	fmt.Println("        Welcome to the RPN Calculator   ")
-	fmt.Println("=========================================")
-	fmt.Println("Functionality:")
-	fmt.Println(" +  Addition")
-	fmt.Println(" -  Subtraction")
-	fmt.Println(" *  Multiplication")
-	fmt.Println(" /  Division")
-	fmt.Println(" ^  Exponentiation")
-	fmt.Println(" sqrt  Square Root")
-	fmt.Println(" log  Logarithm (Base 10)")
-	fmt.Println(" !  Factorial")
-	fmt.Println(" abs  Absolute Value")
-	fmt.Println(" ++  Sum All Numbers on the Stack")
-	fmt.Println(" **  Multiply All Numbers on the Stack")
-	fmt.Println()
-	fmt.Println("Available Commands:")
-	fmt.Println(" - Type 'help' for assistance")
-	fmt.Println(" - Type 'latex' for LaTeX formatted output")
-	fmt.Println(" - Type 'exit' to close the application")
-	fmt.Println()
-	fmt.Println("=========================================")
-	fmt.Println("Please enter your command:")
-}
-
-func getInputByReader() string {
-		reader := bufio.NewReader(os.Stdin)
-		
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Wrong input: ", err)
-			return ""
-		}
-		input = strings.TrimSpace(input)
-		return input
 }
 
 func getInput() string {
@@ -71,124 +25,4 @@ func getInput() string {
 		fmt.Println("Eingabefehler")
 	}
 	return input
-}
-
-func checkInput(input string, numberStack *Stack[float64], latex *LaTeXOutput, history *Stack[string]) {
-
-	// commands
-	if input == "exit" {
-		fmt.Println("\nQuitting Application. See you soon!")
-		os.Exit(0)
-	} else if input == "list" {
-		fmt.Println("printing input")
-		numberStack.Print()
-		return
-	} else if input == "latex" {
-		latex.Expression = history.Top()
-		fmt.Println(history.Top())
-		fmt.Println(latex.Expression)
-		fmt.Println(latex.formatToLatex(latex.Expression))
-		return
-	} else if input == "help" {
-		printWelcomeMessage()
-		return
-	}
-
-	// operators
-	switch input {
-	case "+": 
-		result := numberStack.Pop() + numberStack.Pop()
-		term2 := history.Pop()
-		term1 := history.Pop()
-		termNew := "(" + term1 + "+" +term2+")"
-		history.Push(termNew)
-		fmt.Println("current calculation: " + termNew + " = ", result)
-		numberStack.Push(result)
-	case "-": 
-		secondOp := numberStack.Pop()
-		firstOp := numberStack.Pop()
-		term2 := history.Pop()
-		term1 := history.Pop()
-		termNew := "(" + term1 + "-" +term2+")"
-		history.Push(termNew)
-		result := firstOp - secondOp
-		fmt.Println("current calculation: " + termNew + " = ", result)
-		numberStack.Push(result)
-	case "*": 
-		result := numberStack.Pop() * numberStack.Pop()
-		term2 := history.Pop()
-		term1 := history.Pop()
-		termNew := "(" + term1 + "*" +term2+")"
-		history.Push(termNew)
-		fmt.Println("current calculation: " + termNew + " = ", result)
-		numberStack.Push(result)
-	case "/": 
-		secondOp := numberStack.Pop()
-		firstOp := numberStack.Pop()
-		term2 := history.Pop()
-		term1 := history.Pop()
-		termNew := "(" + term1 + "/" +term2+")"
-		history.Push(termNew)
-		result := firstOp / secondOp
-		fmt.Println("current calculation: " + termNew + " = ", result)
-		numberStack.Push(result)
-	case "abs":
-		result := math.Abs(numberStack.Pop())
-		term1 := history.Pop()
-		termNew := "abs(" + term1 +")"
-		history.Push(termNew)
-		fmt.Println("current calculation: " + termNew + " = ", result)
-		numberStack.Push(result)
-	case "sqrt":
-		result := math.Sqrt(numberStack.Pop())
-		term1 := history.Pop()
-		termNew := "sqrt(" + term1 +")"
-		history.Push(termNew)
-		fmt.Println("current calculation: " + termNew + " = ", result)
-		numberStack.Push(result)
-	case "log":
-		result := math.Log(numberStack.Pop())
-		term1 := history.Pop()
-		termNew := "log(" + term1 +")"
-		history.Push(termNew)
-		fmt.Println("current calculation: " + termNew + " = ", result)
-		numberStack.Push(result)
-	case "^":
-		exponent := numberStack.Pop()
-		base := numberStack.Pop()
-		result := math.Pow(base,exponent)
-		term2 := history.Pop()
-		term1 := history.Pop()
-		termNew := "(" + term1 + "^" +term2+")"
-		history.Push(termNew)
-		fmt.Println("current calculation: " + termNew + " = ", result)
-		numberStack.Push(result)
-	case "++":
-		n := len(*numberStack)
-		result := 0.0
-		// TODO history
-		for i := 0; i < n; i++ {
-			result += numberStack.Pop()
-		}
-		numberStack.Push(result)
-	case "**":
-		n := len(*numberStack)
-		result := 1.0
-		// TODO history
-		for i := 0; i < n; i++ {
-			result *= numberStack.Pop()
-		}
-		if n > 0 {
-			numberStack.Push(result)
-		}
-	}
-
-	// numbers
-	number, err := strconv.ParseFloat(input, 64)
-	if  err != nil {
-		// TODO
-	} else {
-		numberStack.Push(number)
-		history.Push(input)
-	}
 }
