ff97cceba0d09187348cb186c8c612441fd482e1
Till Burdorf <burdorftill@gmail.com>
2025-02-27 15:51:16 +0100

    update

--- linkedList/linkedList.go
+++ linkedList/linkedList.go
@@ -2,13 +2,70 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
-type LinkedList[T any] struct {
+type LinkedList[T comparable] struct {
 	head *Node[T]
 }
 
-// adds a new element at the front of the list 
+// get the node at position pos
+func (list *LinkedList[T]) getNode(pos int) *Node[T] {
+	current := list.head
+	currentPosition := 0
+	for currentPosition < pos {
+		current = current.next
+		currentPosition++
+	}
+	return current
+}
+
+// get the element at position pos
+func (list *LinkedList[T]) getValue(pos int) T {
+	current := list.head
+	currentPosition := 0
+	for currentPosition < pos {
+		current = current.next
+		currentPosition++
+	}
+	return current.data
+}
+
+// add one or multiple elements to the list
+func (list *LinkedList[T]) add(datas ...T) {
+	for _, data := range datas {
+		list.addBack(data)
+	}
+}
+
+// insert an element at a position
+func (list *LinkedList[T]) insert(position int, data T) {
+	current := list.head
+	currentPosition := 0
+	for currentPosition < position-1 {
+		current = current.next
+		currentPosition++
+	}
+	newNode := &Node[T]{data: data, next: current.next}
+	current.next = newNode
+}
+
+// removes the element elem from the list 
+func (list* LinkedList[T]) remove(elem T) {
+	current := list.head
+	for current.data != elem{
+		current = current.next
+	}
+}
+
+// removes the element at position pos from the list 
+func (list* LinkedList[T]) removeAt(pos int){
+	current := list.getNode(pos-1)
+	newNext := list.getNode(pos+1)
+	current.next = newNext
+}
+
+// adds a new element at the front of the list
 func (list *LinkedList[T]) addFront(data T) {
 	newNode := &Node[T]{data: data, next: list.head}
 	list.head = newNode
@@ -40,16 +97,6 @@ func (list *LinkedList[T]) print() {
 	}
 }
 
-// is true if the list is empty
-func (list *LinkedList[T]) isEmpty() bool {
-	return list.head == nil
-}
-
-// is true if the list is not empty 
-func (list *LinkedList[T]) isFull() bool {
-	return list.head != nil
-}
-
 // get the current size of the list
 func (list *LinkedList[T]) size() int {
 	current := list.head
@@ -61,32 +108,12 @@ func (list *LinkedList[T]) size() int {
 	return size
 }
 
-// insert an element at a position
-func (list *LinkedList[T]) insert(position int, data T) {
-	current := list.head
-	currentPosition := 0
-	for currentPosition < position-1 {
-		current = current.next
-		currentPosition++
-	}
-	newNode := &Node[T]{data: data, next: current.next}
-	current.next = newNode
+// is true if the list is empty
+func (list *LinkedList[T]) isEmpty() bool {
+	return list.head == nil
 }
 
-// get the element at position pos
-func (list *LinkedList[T]) get(pos int) T {
-	current := list.head
-	currentPosition := 0
-	for currentPosition < pos {
-		current = current.next
-		currentPosition++
-	}
-	return current.data
-}
-
-// add one or multiple elements to the list
-func (list* LinkedList[T]) add(datas ... T){
-	for _, data := range datas {
-        list.addBack(data)
-    }
+// is true if the list is not empty
+func (list *LinkedList[T]) isFull() bool {
+	return list.head != nil
 }
--- linkedList/main.go
+++ linkedList/main.go
@@ -7,9 +7,10 @@ import (
 func main() {
 	mylist := new(LinkedList[int])
 	mylist.add(10, 11, 12, 13, 14, 15)
-	fmt.Println("Element at index 2:", mylist.get(2))
+	fmt.Println("Element at index 2:", mylist.getValue(2))
 	fmt.Println("Länge der Liste", mylist.size())
 	fmt.Println("Liste ist leer", mylist.isEmpty())
 	fmt.Println("Länge der voll", mylist.isFull())
+	mylist.removeAt(2)
 	mylist.print()
 }
