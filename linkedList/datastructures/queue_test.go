f1df91f1e94a31cb5e54535fd37dddcfab6a69a8
Lukas Groening <113829864+yb981@users.noreply.github.com>
2025-03-10 22:59:21 +0100

    100 percent test coverage

--- linkedList/datastructures/queue_test.go
+++ linkedList/datastructures/queue_test.go
@@ -82,7 +82,7 @@ func TestQueue_IsEmpty(t *testing.T) {
 	}
 }
 
-func TestQueue_IsFull(t *testing.T) {
+func TestQueueIsFull(t *testing.T) {
 	queue := Queue[int]{}
 	// Assuming the list implementation does not constrain size, hence always flexible
 	queue.Enqueue(10)
@@ -93,5 +93,77 @@ func TestQueue_IsFull(t *testing.T) {
 	queue.Dequeue()
 	if queue.IsFull() {
 		t.Errorf("Expected IsFull to return false when queue is empty")
+	}
+}
+
+func TestQueueSize(t *testing.T) {
+	tests := []struct {
+		input []int
+		expected int
+	}{
+		{[]int{}, 0},
+		{[]int{1,2}, 2},
+	}
+
+	for _, test := range tests {
+		queue := Queue[int]{}
+		for _, item := range test.input {
+			queue.Enqueue(item)
+		}
+		result := queue.Size()
+		if result != test.expected {
+			t.Errorf("Expected size: %v, but got %v", test.expected, result)
+		}
+	}
+}
+
+func TestQueueToString(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected string
+	}{
+		{[]int{}, "[]"},
+		{[]int{1}, "[1]"},
+		{[]int{1, 2, 3}, "[1, 2, 3]"},
+	}
+
+	for _, test := range tests {
+		queue := Queue[int]{}
+		for _, item := range test.input {
+			queue.Enqueue(item)
+		}
+		result := queue.ToString()
+		if result != test.expected {
+			t.Errorf("got %s, want %s", result, test.expected)
+		}
+	}
+}
+
+func TestEqual(t *testing.T) {
+	tests := []struct {
+		first []int
+		second []int
+		expected bool
+	}{
+		{[]int{}, []int{}, true},
+		{[]int{1}, []int{1}, true},
+		{[]int{1, 2}, []int{1, 2, 3}, false},
+		{[]int{1, 2, 3}, []int{1, 2, 3}, true},
+		{[]int{1, 2, 3}, []int{4, 5, 6}, false},
+	}
+
+	for _, test := range tests {
+		list1 := Queue[int]{}
+		for _, item := range test.first {
+			list1.Enqueue(item)
+		}
+		list2 := Queue[int]{}
+		for _, item := range test.second {
+			list2.Enqueue(item)
+		}
+		result := list1.Equals(&list2)
+		if result != test.expected {
+			t.Errorf("Equals(%v, %v) got %v, want %v", test.first, test.second, result, test.expected)
+		}
 	}
 }
--- linkedList/datastructures/stack_test.go
+++ linkedList/datastructures/stack_test.go
@@ -20,6 +20,18 @@ func TestStack_Push(t *testing.T) {
 	}
 }
 
+func TestStackPushAll(t *testing.T) {
+	stack1 := Stack[int]{}
+	stack1.PushAll(1, 2, 3)
+	stack2 := Stack[int]{}
+	stack2.Push(1)
+	stack2.Push(2)
+	stack2.Push(3)
+	if !stack1.Equals(&stack2) {
+		t.Errorf("Expected stacks to be the same. Stack1: %v, Stack2: %v", stack1.ToString(), stack2.ToString())
+	}
+}
+
 func TestStack_Pop(t *testing.T) {
 	stack := Stack[int]{}
 	stack.Push(10)
@@ -116,5 +128,56 @@ func TestStack_Size(t *testing.T) {
 	stack.Pop()
 	if stack.Size() != 1 {
 		t.Errorf("Expected stack size to be 1 after pop, got %v", stack.Size())
+	}
+}
+
+func TestStackToString(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected string
+	}{
+		{[]int{}, "[]"},
+		{[]int{1}, "[1]"},
+		{[]int{1, 2, 3}, "[3, 2, 1]"},
+	}
+
+	for _, test := range tests {
+		stack := Stack[int]{}
+		for _, item := range test.input {
+			stack.Push(item)
+		}
+		result := stack.ToString()
+		if result != test.expected {
+			t.Errorf("got %s, want %s", result, test.expected)
+		}
+	}
+}
+
+func TestStackEqual(t *testing.T) {
+	tests := []struct {
+		first []int
+		second []int
+		expected bool
+	}{
+		{[]int{}, []int{}, true},
+		{[]int{1}, []int{1}, true},
+		{[]int{1, 2}, []int{1, 2, 3}, false},
+		{[]int{1, 2, 3}, []int{1, 2, 3}, true},
+		{[]int{1, 2, 3}, []int{4, 5, 6}, false},
+	}
+
+	for _, test := range tests {
+		list1 := Stack[int]{}
+		for _, item := range test.first {
+			list1.Push(item)
+		}
+		list2 := Stack[int]{}
+		for _, item := range test.second {
+			list2.Push(item)
+		}
+		result := list1.Equals(&list2)
+		if result != test.expected {
+			t.Errorf("Equals(%v, %v) got %v, want %v", test.first, test.second, result, test.expected)
+		}
 	}
 }
