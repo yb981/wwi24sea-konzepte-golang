b47981e6449fd266c6a1c3fa9a5a0ffa7b350bdf
Till Burdorf <burdorftill@gmail.com>
2025-03-13 11:47:38 +0100

    update

--- linkedList/datastructures/functionalProgramming.go
+++ linkedList/datastructures/functionalProgramming.go
@@ -1,28 +1,52 @@
 package datastructures
 
+
+//-----------------------------------------------------------------------------------------
 // For Each Method
-func (list *LinkedList[T]) ForEach(operation func(T)){
+//-----------------------------------------------------------------------------------------
+func (list *LinkedList[T]) ForEach(operation func(T)) {
 	current := list.head
 	for current != nil {
 		operation(current.data)
 		current = current.next
-    }
+	}
 }
 
-// Filter Method 
-func (list *LinkedList[T]) Filter(operation func(T) bool) LinkedList[T]{
-    current := list.head
+func (queue *Queue[T]) ForEach(operation func(T)) {
+	queue.list.ForEach(operation)
+}
+
+func (stack *Stack[T]) ForEach(operation func(T)) {
+	stack.list.ForEach(operation)
+}
+//-----------------------------------------------------------------------------------------
+
+
+
+
+//-----------------------------------------------------------------------------------------
+// Filter Method
+//-----------------------------------------------------------------------------------------
+func (list *LinkedList[T]) Filter(operation func(T) bool) LinkedList[T] {
+	current := list.head
 	newList := &LinkedList[T]{}
 	for current != nil {
-        if operation(current.data){
-            newList.Append(current.data)
-        }
+		if operation(current.data) {
+			newList.Append(current.data)
+		}
 		current = current.next
 	}
 	return *newList
 }
+//-----------------------------------------------------------------------------------------
 
+
+
+
+
+//-----------------------------------------------------------------------------------------
 // Map Method
+//-----------------------------------------------------------------------------------------
 func (list *LinkedList[T]) Map(operation func(T) T) LinkedList[T] {
 	current := list.head
 	newList := &LinkedList[T]{}
@@ -45,3 +69,4 @@ func (stack *Stack[T]) Map(operation func(T) T) Stack[T] {
 	newStack.list = stack.list.Map(operation)
 	return *newStack
 }
+//-----------------------------------------------------------------------------------------
