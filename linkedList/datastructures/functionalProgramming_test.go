484145954a76b3d92f05db3751f8d31783c345f9
Lukas Groening <113829864+yb981@users.noreply.github.com>
2025-03-14 11:38:08 +0100

    added filter test

--- linkedList/datastructures/functionalProgramming_test.go
+++ linkedList/datastructures/functionalProgramming_test.go
@@ -33,3 +33,34 @@ func TestForEach(t *testing.T) {
 		}
 	}
 }
+
+func TestFilter(t *testing.T) {
+	isEvenFunc := func (a int) bool {
+		return a % 2 == 0
+	}
+
+	tests := []struct {
+		input 		[]int
+		expected 	[]int
+	}{
+		{[]int{1,2,3}, []int{2} },
+		{[]int{-3,-2, -1}, []int{-2} },
+		{[]int{50,-2, 20}, []int{50, -2, 20} },
+	}
+
+	for _, test := range tests {
+		var testList LinkedList[int]
+		for _, v := range test.input {
+			testList.Append(v)
+		}
+		testList = testList.Filter(isEvenFunc)
+
+		var resultList LinkedList[int]
+		for _, v := range test.expected {
+			resultList.Append(v)
+		}
+		if !testList.Equals(&resultList) {
+			t.Errorf("Expected %v, but got %v", resultList.ToString(), testList.ToString())
+		}
+	}
+}
