3f81b3b07df828368ec03d11912ac5db62eebb9c
Lukas Groening <113829864+yb981@users.noreply.github.com>
2025-03-14 11:00:14 +0100

    for each test

---
+++ linkedList/datastructures/functionalProgramming_test.go
@@ -0,0 +1,35 @@
+package datastructures
+
+import (
+	"testing"
+)
+
+func TestForEach(t *testing.T) {
+	testfunc := func (a int) {
+		a = a + 2
+	}
+
+	tests := []struct {
+		input 		[]int
+		expected 	[]int
+	}{
+		{[]int{1,2,3}, []int{3,4,5} },
+		{[]int{-3,-2, -1}, []int{-1,0,1} },
+	}
+
+	for _, test := range tests {
+		var testList LinkedList[int]
+		for _, v := range test.input {
+			testList.Append(v)
+		}
+		testList.ForEach(testfunc)
+
+		var resultList LinkedList[int]
+		for _, v := range test.expected {
+			resultList.Append(v)
+		}
+		if !testList.Equals(&resultList) {
+			t.Errorf("Expected %v, but got %v", resultList.ToString(), testList.ToString())
+		}
+	}
+}
