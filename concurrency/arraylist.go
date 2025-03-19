3a9c1485a67401c4e76e3aed3fbfa971903ba4a1
Till Burdorf <burdorftill@gmail.com>
2025-03-19 14:13:38 +0100

    parallel map function

--- concurrency/arraylist.go
+++ concurrency/arraylist.go
@@ -1,12 +1,29 @@
 package main
 
-type ArrayList[T comparable] struct{
-	list[] T
+import (
+	"sync"
+)
+
+type ArrayList[T comparable] struct {
+	list []T
 }
 
-func(al* ArrayList[T]) Append(value T){
+func (al *ArrayList[T]) Append(value T) {
 	al.list = append(al.list, value)
 }
 
-func(al* ArrayList[T]) Insert(value T){
+
+func (al *ArrayList[T]) parallelMap(f func(T) T) *ArrayList[T] {
+	var wg sync.WaitGroup
+	newList := &ArrayList[T]{list: make([]T, len(al.list))}
+
+	for i, v := range al.list {
+		wg.Add(1)
+		go func(i int, v T) {
+			defer wg.Done()
+			newList.list[i] = f(v)
+		}(i, v)
+	}
+	wg.Wait()
+	return newList
 }
--- concurrency/main.go
+++ concurrency/main.go
@@ -1,9 +1,19 @@
 package main
-import(
+
+import (
 	"fmt"
 )
 
-func main(){
+func doubleValue(input int) int {
+	return input * 2
+}
+
+func main() {
 	myList := &ArrayList[int]{}
-	
+	for i := range 10 {
+		myList.Append(i)
+	}
+	parallelList := myList.parallelMap(doubleValue)
+
+	fmt.Println(parallelList)
 }
